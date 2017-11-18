de507c01015f8c8fefeeac9e89b8b288b1e67e14
dmitrylych <dmitrylych@gmail.com>
2017-11-19 00:27:47 +0300

    feat:test1

--- src/main/java/lych/trucks/application/controller/DriverLicenseController.java
+++ src/main/java/lych/trucks/application/controller/DriverLicenseController.java
@@ -1,4 +1,16 @@
 package lych.trucks.application.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DriverLicenseController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity hello() {
+
+        return ResponseEntity.ok().body("HelloWorld");
+    }
 }
