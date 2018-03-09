2cb5b9c56745242ea371a3ef9a6ce84161dbf288
Dmitry Lych <dmitrylych@gmail.com>
2018-03-09 17:21:20 +0300

    feat: cross origin

--- src/main/java/lych/trucks/application/security/controller/AuthController.java
+++ src/main/java/lych/trucks/application/security/controller/AuthController.java
@@ -8,6 +8,7 @@ import lych.trucks.application.security.service.jwt.TokenService;
 import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import static lych.trucks.application.security.dto.TokenResponse.aTokenResponse;
  */
 @RequestMapping(value = "/cargo/v1/auth")
 @RestController
+@CrossOrigin
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AuthController {
 
